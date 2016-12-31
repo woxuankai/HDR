de3e6765774e79455c49899b2f800f1190621114
Kyle Xuan <woxuankai@gmail.com>
2016-12-31 13:38:26 +0800

    parallelized hdr in part of loops

--- hdr/main.cpp
+++ hdr/main.cpp
@@ -1,15 +1,15 @@
+#include <iostream>
+#include <cstdint>
+#include <tbb/tbb.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
-#include <cstdint>
-#include <semaphore.h>
-#include <pthread.h>
+//#include "timecal.h"
+
+#define HDR_USE_TBB
+
 using namespace std;
 using namespace cv;
-
-
-int threadnum = 1;
 
 
 void cal_L2R_table(float* table,float Rmax, float n, float alpha, float beta);
@@ -25,25 +25,57 @@ void cal_BGR(const Mat& Lcone,\
 	     float *table_Lconepownegatives);
 
 
-#define TIMESTAMP(id)				\
+
+#define TIMER_STAMP_I(TIMER, ID)		\
+  static double __##TIMER##_total_##ID = 0;		\
+  static double __##TIMER##_count_##ID = 0;			
+
+#define TIMER_INIT(TIMER) TIMER_STAMP_I(TIMER, timer)
+
+#define TIMER_START(TIMER)				\
+  double __##TIMER##_start = (double)getTickCount();	\
+  double __##TIMER##_last = __##TIMER##_start;	
+  
+#define TIMER_STAMP(TIMER, ID)					\
+  {								\
+    double temptime = (double)getTickCount();			\
+    __##TIMER##_total_##ID += temptime - __##TIMER##_last;	\
+    __##TIMER##_count_##ID += 1;				\
+    __##TIMER##_last = temptime;				\
+  }
+
+
+#define TIMER_STOP(TIMER)					\
+  {								\
+    double temptime = (double)getTickCount();			\
+    __##TIMER##_total_timer += temptime - __##TIMER##_start;	\
+    __##TIMER##_count_timer += 1;				\
+  }
+
+#define TIMER_STAMP_P(TIMER, ID)				\
+  {								\
+    cout << #TIMER #ID "("					\
+	 <<__##TIMER##_count_##ID				\
+	 <<"times) : "						\
+	 << __##TIMER##_total_##ID/__##TIMER##_count_##ID	\
+      /getTickFrequency()*1000					\
+	 << endl;						\
+  }
+
+
+#define TIMER_PRINT(TIMER)			\
   {						\
-    double temptime = (double)getTickCount();	\
-    cout << #id << ": "				\
-	 << (temptime - lasttime )		\
-      / getTickFrequency() * 1000 << endl;	\
-    lasttime = temptime ;			\
-  }						\
-
-
+    TIMER_STAMP_P(TIMER, timer);		\
+  }
 
 float alpha = 0.67;
 float beta_cone = 4;
 float beta_rod = 2;
-float n=0.8;
-float R_max=2.5;
-float KK=2.5;
-float t=0.1;
-float s=0.8;
+float n = 0.8;
+float R_max = 2.5;
+float KK = 2.5;
+float t = 0.1;
+float s = 0.8;
 
 #define IMG_WIDTH 640
 #define IMG_HEIGHT 480
@@ -65,52 +97,20 @@ float table_Lcone2a[UINT16_MAX+1] = {0};
 float table_Lconepownegatives[UINT16_MAX+1] = {0};
 
 
-void init_hdr()
-{
-  cal_L2R_table(table_L2R_cone, R_max, n, alpha, beta_cone);
-  cal_L2R_table(table_L2R_rod, R_max, n, alpha, beta_rod);
-  cal_Lcone2a_table(table_Lcone2a, t);
-  cal_Lconepownegatives_table(table_Lconepownegatives, s);
-
-  hh = (getGaussianKernel(21,1,CV_32F) - getGaussianKernel(21,4,CV_32F))*KK;
-  hh.at<float>(1,10) += 1;
-  return;
-}
-
-void do_hdr(Mat &origImg)
-{
-  ////////////////////////////infinite loop///////////////////////////////////
-  double timestart = (double)getTickCount();
-  double lasttime = timestart;
-
-  cal_Lcone_Lrod(origImg, L_cone, L_rod);
-  TIMESTAMP(rod_cone);
-
-  cal_R(L_cone, R_cone, table_L2R_cone);
-  cal_R(L_rod, R_rod, table_L2R_rod);
-  TIMESTAMP(genR);
+void init_hdr();
+void do_hdr(Mat &origImg);
 
 
-  filter2D(R_cone, DOG_cone, CV_32F, hh);
-  filter2D(R_rod, DOG_rod, CV_32F, hh);
-  TIMESTAMP(dofilt);
-
- 
-  cal_BGR(L_cone, DOG_cone, DOG_rod, origImg,\
-	  table_Lcone2a,table_Lconepownegatives);
-  TIMESTAMP(calBGR);
- 
-  timestart  = ((double)getTickCount() - timestart ) / getTickFrequency() * 1000;
-  cout << "time cost : "<< timestart << endl;
-  ////////////////////////////end infinite loop///////////////////////////////////
-  return;
-}
+TIMER_INIT(HDR);
+TIMER_STAMP_I(HDR,LL);
+TIMER_STAMP_I(HDR,L2R);
+TIMER_STAMP_I(HDR,FILT);
+TIMER_STAMP_I(HDR,BGR);
 
 
 
 int main(int argc, char* argv[])
 {
-
   int ifvideo = 0;
   VideoCapture cap(0);
   Mat origImg;
@@ -163,8 +163,56 @@ int main(int argc, char* argv[])
 	  break;
 	}
     }
+
+  TIMER_STAMP_P(HDR,LL);
+  TIMER_STAMP_P(HDR,L2R);
+  TIMER_STAMP_P(HDR,FILT);
+  TIMER_STAMP_P(HDR,BGR);
+  TIMER_PRINT(HDR);
   return 0;
 }
+
+
+
+
+void init_hdr()
+{
+  cal_L2R_table(table_L2R_cone, R_max, n, alpha, beta_cone);
+  cal_L2R_table(table_L2R_rod, R_max, n, alpha, beta_rod);
+  cal_Lcone2a_table(table_Lcone2a, t);
+  cal_Lconepownegatives_table(table_Lconepownegatives, s);
+
+  hh = (getGaussianKernel(21,1,CV_32F) - getGaussianKernel(21,4,CV_32F))*KK;
+  hh.at<float>(1,10) += 1;
+  return;
+}
+
+void do_hdr(Mat &origImg)
+{
+  TIMER_START(HDR);
+  
+  cal_Lcone_Lrod(origImg, L_cone, L_rod);
+  TIMER_STAMP(HDR,LL);
+
+  cal_R(L_cone, R_cone, table_L2R_cone);
+  cal_R(L_rod, R_rod, table_L2R_rod);
+  TIMER_STAMP(HDR,L2R);
+
+  filter2D(R_cone, DOG_cone, CV_32F, hh);
+  filter2D(R_rod, DOG_rod, CV_32F, hh);
+  TIMER_STAMP(HDR,FILT);
+ 
+  cal_BGR(L_cone, DOG_cone, DOG_rod, origImg,\
+	  table_Lcone2a,table_Lconepownegatives);
+  TIMER_STAMP(HDR,BGR);
+
+  TIMER_STOP(HDR);
+
+  return;
+}
+
+
+
 
 
 void cal_Lcone_Lrod(const Mat& srcBGR, Mat& Lcone, Mat& Lrod)
@@ -191,6 +239,7 @@ void cal_Lcone_Lrod(const Mat& srcBGR, Mat& Lcone, Mat& Lrod)
   int nRows = srcBGR.rows;
   int nCols = srcBGR.cols;
 
+#ifndef HDR_USE_TBB
   if ((srcBGR.isContinuous())&&\
       (Lcone.isContinuous())&&\
       (Lrod.isContinuous()))
@@ -198,17 +247,20 @@ void cal_Lcone_Lrod(const Mat& srcBGR, Mat& Lcone, Mat& Lrod)
       nCols *= nRows;
       nRows = 1;
     }
-  int i,j;
-  const uint8_t *psrc;
-  uint16_t *pcone, *prod;
 
-  for( i = 0; i < nRows; ++i)
+  for(int i = 0; i < nRows; ++i)
+#else
+    tbb::parallel_for(0, nRows, [&](int i)
+#endif
     {
+      const uint8_t *psrc;
+      uint16_t *pcone, *prod;
+
       psrc = srcBGR.ptr<uint8_t>(i);
       pcone = Lcone.ptr<uint16_t>(i);
       prod = Lrod.ptr<uint16_t>(i);
       float temp;
-      for ( j = 0; j < nCols; ++j)
+      for (int j = 0; j < nCols; ++j)
         {
 	  int srcj = j * channels;
 	  temp = bgr2cone[0]*psrc[srcj]\
@@ -231,7 +283,11 @@ void cal_Lcone_Lrod(const Mat& srcBGR, Mat& Lcone, Mat& Lrod)
 	    temp = UINT16_MAX;
 	  prod[j] = temp;
         }
+#ifdef HDR_USE_TBB
+    });
+#else
     }
+#endif
 }
 
 void cal_R(const Mat& L, Mat& R, float* table)
@@ -349,6 +405,7 @@ void cal_BGR(const Mat& Lcone,\
   int nRows = BGR.rows;
   int nCols = BGR.cols;
 
+#ifndef HDR_USE_TBB
   if ((BGR.isContinuous())&&\
       (Lcone.isContinuous())&&\
       (DOGcone.isContinuous())&&\
@@ -357,20 +414,22 @@ void cal_BGR(const Mat& Lcone,\
       nCols *= nRows;
       nRows = 1;
     }
-  int i,j;
-  uint8_t *pBGR;
-  const uint16_t *pLcone;
-  const float *pDOGcone, *pDOGrod;
-  float tempbgr;
 
-  for( i = 0; i < nRows; ++i)
+  for(int i = 0; i < nRows; ++i)
+#else
+  tbb::parallel_for(0, nRows,[&](int i)
+#endif
     {
+      uint8_t *pBGR;
+      const uint16_t *pLcone;
+      const float *pDOGcone, *pDOGrod;
+      float tempbgr;
       pBGR = BGR.ptr<uint8_t>(i);
       pLcone = Lcone.ptr<uint16_t>(i);
       pDOGcone = DOGcone.ptr<float>(i);
       pDOGrod = DOGrod.ptr<float>(i);
 
-      for ( j = 0; j < nCols; ++j)
+      for (int j = 0; j < nCols; ++j)
         {
 	  int BGRj = j * channels;
 	  uint16_t Lconevalue;
@@ -410,8 +469,12 @@ void cal_BGR(const Mat& Lcone,\
 	  if(tempbgr > UINT8_MAX)
 	    tempbgr = UINT8_MAX;
 	  pBGR[BGRj+2] = tempbgr;
-        }
+	}
+#ifdef HDR_USE_TBB
+    });
+#else
     }
+#endif
   return;
 }
 
