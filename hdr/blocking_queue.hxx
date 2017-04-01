5622a2089b1bc2c0553ac1fd8a0b4f34d02f5c1f
Kyle Xuan <woxuankai@gmail.com>
2017-04-01 12:23:55 +0800

    link passed

--- hdr/blocking_queue.hxx
+++ hdr/blocking_queue.hxx
@@ -7,14 +7,25 @@
 #include <functional>
 #include <iostream>
 
-template<typename Alloc>
+template<class Alloc>
 class blocking_queue{//: protected std::queue{//too many functions to implement
 public:
   typedef typename std::queue<Alloc>::size_type size_type;
-  blocking_queue();
-  ~blocking_queue();
-  size_type put(const Alloc &); // return queue length after put
-  size_type get(Alloc &); // return queue length after get
+  blocking_queue(){};
+  ~blocking_queue(){};
+  size_type put(const Alloc &obj){ // return queue length after put
+    std::unique_lock<std::mutex> lck(mtx_);
+    queue_.push(obj);
+    cond_var_.notify_all();
+    return queue_.size(); // the mutex is still locked now
+  }
+  size_type get(Alloc &obj){ // return queue length after get
+    std::unique_lock<std::mutex> lck(mtx_);
+    while(queue_.empty()) cond_var_.wait(lck);
+    obj = queue_.front();
+    queue_.pop();
+    return queue_.size();
+  }
 protected:
   std::mutex mtx_; // queue mutex
   std::queue<Alloc> queue_;
