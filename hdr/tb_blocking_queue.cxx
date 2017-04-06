00e731849dace2ac50c16fc7b31bc7e9ff9f79b4
Kyle Xuan <woxuankai@gmail.com>
2017-04-06 11:15:38 +0800

    added testbench for blocking_queue

--- hdr/tb_blocking_queue.cxx
+++ hdr/tb_blocking_queue.cxx
@@ -1,26 +1,58 @@
 #include <iostream>
 #include <chrono>
+#include <opencv2/opencv.hpp>
 #include "blocking_queue.hxx"
 
 
-const int maxrepeat=1000;
+
+const int maxrepeat=100;
 const int maxlength=30;
 cv::Size imagesize(640,480);
-auto image = cv::Mat::ones(Size,cv::CV_8UC3)
+cv::Mat image = cv::Mat::ones(imagesize,CV_8UC3);
+cv::Mat tmpimage = image;
+
+typedef typename std::chrono::steady_clock clocktype;
+typedef typename clocktype::duration durationtype;
+typedef typename clocktype::time_point timepointtype;
+durationtype time_clone(0);
+durationtype time_put(0);
+durationtype time_get(0);
+timepointtype start = clocktype::now();
+timepointtype stop = start;
+
+//clocktype::time_point start = clocktype::now();
+
 
 int main(int argc, char* argv[]){
   blocking_queue<cv::Mat> q;
-  for(int repeatcnt = 0; repeatcnt < maxrepeat; repeatcnt++)
-    for(int lengthcnt = 0; lengthcnt < maxlength; lengthcnt++)
-      clone
-      q.put(imagecopy);
-  q.put(123214);
-  q.put(123214);
-  int out;
-  while(q.get(out)){
-    std::cout << out << std::endl;
-  };
-  std::cout << "end" << std::endl;
+  for(int repeatcnt = 0; repeatcnt < maxrepeat; repeatcnt++){
+    for(int lengthcnt = 0; lengthcnt < maxlength; lengthcnt++){
+      start = clocktype::now();
+      tmpimage = image.clone();
+      stop = clocktype::now();
+      time_clone += stop - start;
+      start = clocktype::now();
+      q.put(tmpimage);
+      stop = clocktype::now();
+      time_put += stop - start;
+    }
+    for(int lengthcnt = 0; lengthcnt < maxlength; lengthcnt++){
+      start = clocktype::now();
+      q.get(tmpimage);
+      stop = clocktype::now();
+      time_get += stop - start;
+    }
+  }
+  auto totalrepeats = maxlength * maxrepeat;
+  typedef typename std::chrono::microseconds disptype;
+  std::cout << "repeat " << totalrepeats << \
+      " times, in microseconds" << std::endl;
+  std::cout << "clone: " << \
+      std::chrono::duration_cast<disptype>(time_clone).count() << std::endl;
+  std::cout << "put  : " << \
+      std::chrono::duration_cast<disptype>(time_put).count() << std::endl;
+  std::cout << "get  : " << \
+      std::chrono::duration_cast<disptype>(time_get).count() << std::endl;
   return 0;
 }
 
